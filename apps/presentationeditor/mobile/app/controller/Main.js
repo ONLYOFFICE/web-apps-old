96382957d89453c762d0acca7389119638bfcacd
Julia Radzhabova <Julia.Radzhabova@onlyoffice.com>
2016-05-24 14:59:04 +0300

    Отладка мобильных версий - не грузился конфиг.

--- apps/presentationeditor/mobile/app/controller/Main.js
+++ apps/presentationeditor/mobile/app/controller/Main.js
@@ -92,6 +92,7 @@ Ext.define('PE.controller.Main', {
     },
 
     loadConfig: function(data) {
+        this.editorConfig = Ext.merge(this.editorConfig, data.config);
         this.editorConfig.user = this._fillUserInfo(data.config.user, this.editorConfig.lang, this.textAnonymous);
     },
 
