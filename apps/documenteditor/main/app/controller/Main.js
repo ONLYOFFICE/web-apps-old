2b1e284eb4ce6c9cb27b818888d48894502161e9
Julia Radzhabova <Julia.Radzhabova@onlyoffice.com>
2017-09-22 17:18:06 +0300

    [DE] Set serverVersion parameter to version history items.

--- apps/documenteditor/main/app/controller/Main.js
+++ apps/documenteditor/main/app/controller/Main.js
@@ -447,7 +447,8 @@ define([
                                     markedAsVersion: (group!==version.versionGroup),
                                     selected: (opts.data.currentVersion == version.version),
                                     canRestore: this.appOptions.canHistoryRestore && (ver < versions.length-1),
-                                    isExpanded: true
+                                    isExpanded: true,
+                                    serverVersion: version.serverVersion
                                 }));
                                 if (opts.data.currentVersion == version.version) {
                                     currentVersion = arrVersions[arrVersions.length-1];
@@ -496,7 +497,8 @@ define([
                                                 selected: false,
                                                 canRestore: this.appOptions.canHistoryRestore,
                                                 isRevision: false,
-                                                isVisible: true
+                                                isVisible: true,
+                                                serverVersion: version.serverVersion
                                             }));
                                             arrColors.push(user.get('colorval'));
                                         }
